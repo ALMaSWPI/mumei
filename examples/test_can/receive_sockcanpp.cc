26d0cb689536c4d0f9d52aca94d2c07656c39c73
Duc Doan <dtbpkmte@gmail.com>
2023-09-17 16:10:03 -0400

    Update CAN tests

--- examples/test_can/receive_sockcanpp.cc
+++ examples/test_can/receive_sockcanpp.cc
@@ -1,22 +1,26 @@
 #include <iostream>
 #include "CanDriver.hpp"
 
-void receiveCanFramesExample() {
-    sockcanpp::CanDriver canDriver("can1", CAN_RAW);
+void ReceiveCanFramesExample() {
+  sockcanpp::CanDriver canDriver("can1", CAN_RAW);
 
-    if (canDriver.waitForMessages(sockcanpp::milliseconds(3000) /* timeout */)) {
-        // read a single message
-        sockcanpp::CanMessage receivedMessage = canDriver.readMessage();
+  if (canDriver.waitForMessages(sockcanpp::milliseconds(3000) /* timeout */)) {
+    // read a single message
+    sockcanpp::CanMessage receivedMessage = canDriver.readMessage();
 
-        // read all available messages
-        sockcanpp::queue<sockcanpp::CanMessage> receivedMessages = canDriver.readQueuedMessages();
+    // read all available messages
+    // sockcanpp::queue<sockcanpp::CanMessage> receivedMessages = canDriver.readQueuedMessages();
 
-        // handle CAN frames
-        std::cout << receivedMessage.getFrameData() << std::endl;
-    }
+    // handle CAN frames
+    std::cout << "Message data:" << std::endl;
+    std::cout << "\tCAN ID: "
+              << uint32_t(receivedMessage.getCanId()) << std::endl;
+    std::cout <<  "\tFrame Data: " 
+              << receivedMessage.getFrameData() << std::endl;
+  }
 }
 
 int main(int argc, char* argv[]) { 
-    receiveCanFramesExample();
-    return 0;
+  ReceiveCanFramesExample();
+  return 0;
 }
