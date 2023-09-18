a7794ec687d797c5b511c9ca12ad09d5fceac090
Duc Doan <dtbpkmte@gmail.com>
2023-09-17 21:56:11 -0400

    Fix template type

--- examples/test_huron_canbus/send_msg.cc
+++ examples/test_huron_canbus/send_msg.cc
@@ -17,22 +17,24 @@ int main(int argc, char* argv[]) {
 	msg.id += HuronODriveCAN::MSG_SET_CONTROLLER_MODES;
 	msg.isExt = false;
 	msg.len = 8;
-  can_setSignal<float>(msg, CONTROL_MODE_TORQUE_CONTROL, 0, 32, true);
-  can_setSignal<float>(msg, INPUT_MODE_PASSTHROUGH, 32, 32, true);
+  can_setSignal<int32_t>(msg, CONTROL_MODE_TORQUE_CONTROL, 0, 32, true);
+  can_setSignal<int32_t>(msg, INPUT_MODE_PASSTHROUGH, 32, 32, true);
   hcb.send_message(msg);
   std::cout << "Control mode and input mode set.\n";
+
+  std::this_thread::sleep_for(2s);
 
 	msg.id = hcb.axis_id_ << HuronODriveCAN::NUM_CMD_ID_BITS;
 	msg.id += HuronODriveCAN::MSG_SET_INPUT_TORQUE;
 	msg.isExt = false;
 	msg.len = 8;
-  can_setSignal<float>(msg, 0.7, 32, 32, true);
+  can_setSignal<float>(msg, 0.7, 0, 32, true);
   hcb.send_message(msg);
   std::cout << "Sending torque...\n";
 
   std::this_thread::sleep_for(2s);
 
-  can_setSignal<float>(msg, 0.0, 32, 32, true);
+  can_setSignal<float>(msg, 0.0, 0, 32, true);
   hcb.send_message(msg);
   std::cout << "Stopping...\n";
 
