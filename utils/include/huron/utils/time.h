a933d59e6be07abfe677d43cf62d183baf13d65d
Duc Doan <dtbpkmte@gmail.com>
2023-09-18 14:48:41 -0400

    Add utils

---
+++ utils/include/huron/utils/time.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <chrono>
+
+// Source: https://stackoverflow.com/questions/2808398/easily-measure-elapsed-time
+template <
+    class result_t   = std::chrono::milliseconds,
+    class clock_t    = std::chrono::steady_clock,
+    class duration_t = std::chrono::milliseconds
+>
+auto since(std::chrono::time_point<clock_t, duration_t> const& start)
+{
+    return std::chrono::duration_cast<result_t>(clock_t::now() - start);
+}
+
