dbeb0f25b4d97948b4d815becbd382fb61d4ce37
Duc Doan <dtbpkmte@gmail.com>
2023-09-18 17:12:31 -0400

    Fix

--- examples/test_odrive_can/read_encoder.cc
+++ examples/test_odrive_can/read_encoder.cc
@@ -7,7 +7,11 @@ int main(int argc, char* argv[]) {
   HURONCanBus hcb{"can0", 0};
   HuronODriveCAN hoc{&hcb, 0};
   int32_t shadow_cnt = 0, cnt_cpr = 0;
-  hoc.GetEncoderCount(shadow_cnt, cnt_cpr);
+  bool success = hoc.GetEncoderCount(shadow_cnt, cnt_cpr);
+  if (!success)
+    std::cout << "Get encoder count failed.\n";
+  else
+    std::cout << "Get encoder count succesful.\n";
   std::cout << "Shadow count: " << shadow_cnt
             << "\tCount in CPR: " << cnt_cpr << std::endl;
   return 0;
