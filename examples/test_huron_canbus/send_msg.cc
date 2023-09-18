d27fecb3a86206da624bcbdc29680facff6d9bb7
Duc Doan <dtbpkmte@gmail.com>
2023-09-17 21:46:36 -0400

    Fix send torque

--- examples/test_huron_canbus/send_msg.cc
+++ examples/test_huron_canbus/send_msg.cc
@@ -1,27 +1,40 @@
 #include <iostream>
 #include <huron_driver/can/huron_canbus.h>
 #include <huron_driver/can/huron_odrive_can.h>
+#include <huron_driver/config/config.h>
 #include <chrono>
 #include <thread>
 
 using namespace std::chrono_literals;
 
-bool SendMsg(uint32_t msg_id, float val) {
+int main(int argc, char* argv[]) { 
   HURONCanBus hcb{"can0", 0};
   std::cout << "HURONCanBus initialized successfully.\n";
-
   can_Message_t msg;
+
+  // Set input mode & control mode
 	msg.id = hcb.axis_id_ << HuronODriveCAN::NUM_CMD_ID_BITS;
-	msg.id += msg_id;
+	msg.id += HuronODriveCAN::MSG_SET_CONTROLLER_MODES;
 	msg.isExt = false;
 	msg.len = 8;
-  can_setSignal<float>(msg, val, 0, 32, true);
-  return hcb.send_message(msg);
-}
+  can_setSignal<float>(msg, CONTROL_MODE_TORQUE_CONTROL, 0, 32, true);
+  can_setSignal<float>(msg, INPUT_MODE_PASSTHROUGH, 32, 32, true);
+  hcb.send_message(msg);
+  std::cout << "Control mode and input mode set.\n";
 
-int main(int argc, char* argv[]) { 
-  SendMsg(HuronODriveCAN::MSG_SET_INPUT_TORQUE, 0.7);
+	msg.id = hcb.axis_id_ << HuronODriveCAN::NUM_CMD_ID_BITS;
+	msg.id += HuronODriveCAN::MSG_SET_INPUT_TORQUE;
+	msg.isExt = false;
+	msg.len = 8;
+  can_setSignal<float>(msg, 0.7, 32, 32, true);
+  hcb.send_message(msg);
+  std::cout << "Sending torque...\n";
+
   std::this_thread::sleep_for(2s);
-  SendMsg(HuronODriveCAN::MSG_SET_INPUT_TORQUE, 0.0);
+
+  can_setSignal<float>(msg, 0.0, 32, 32, true);
+  hcb.send_message(msg);
+  std::cout << "Stopping...\n";
+
   return 0;
 }
