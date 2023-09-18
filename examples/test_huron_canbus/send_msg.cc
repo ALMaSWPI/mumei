d39176d25fa6227943340a7c01bda7029faa610b
Duc Doan <dtbpkmte@gmail.com>
2023-09-17 21:21:29 -0400

    Fix

--- examples/test_huron_canbus/send_msg.cc
+++ examples/test_huron_canbus/send_msg.cc
@@ -12,16 +12,16 @@ bool SendMsg(uint32_t msg_id, float val) {
 
   can_Message_t msg;
 	msg.id = hcb.axis_id_ << HuronODriveCAN::NUM_CMD_ID_BITS;
-	msg.id += HuronODriveCAN::MSG_SET_INPUT_TORQUE;
+	msg.id += msg_id;
 	msg.isExt = false;
 	msg.len = 8;
-  can_setSignal(msg, val, 0, 32, true);
+  can_setSignal<float>(msg, val, 0, 32, true);
   return hcb.send_message(msg);
 }
 
 int main(int argc, char* argv[]) { 
-  SendMsg(0, 0.0);
+  SendMsg(HuronODriveCAN::MSG_SET_INPUT_TORQUE, 0.7);
   std::this_thread::sleep_for(2s);
-  SendMsg(0, 0.0);
+  SendMsg(HuronODriveCAN::MSG_SET_INPUT_TORQUE, 0.0);
   return 0;
 }
