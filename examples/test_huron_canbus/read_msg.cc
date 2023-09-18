da5d1ec92ffdbb1de6467e690f86c14eff6ec4a3
Duc Doan <dtbpkmte@gmail.com>
2023-09-18 14:51:48 -0400

    Modify test_huron_canbus to use utils

--- examples/test_huron_canbus/read_msg.cc
+++ examples/test_huron_canbus/read_msg.cc
@@ -1,16 +1,6 @@
 #include <iostream>
 #include <huron_driver/can/huron_canbus.h>
-
-// Source: https://stackoverflow.com/questions/2808398/easily-measure-elapsed-time
-template <
-    class result_t   = std::chrono::milliseconds,
-    class clock_t    = std::chrono::steady_clock,
-    class duration_t = std::chrono::milliseconds
->
-auto since(std::chrono::time_point<clock_t, duration_t> const& start)
-{
-    return std::chrono::duration_cast<result_t>(clock_t::now() - start);
-}
+#include "huron/utils/time.h"
 
 void ReadMessage(uint32_t target_id) {
   HURONCanBus hcb{"can0", 0};
