c94b68ceea5b141798ff84979d553d57ec2186a1
Duc Doan <dtbpkmte@gmail.com>
2023-09-18 11:49:27 -0400

    Add calibration

--- huron_driver/config/include/huron_driver/config/odrive_config.h
+++ huron_driver/config/include/huron_driver/config/odrive_config.h
@@ -2,23 +2,5 @@
 
 #define AXIS_COUNT (2)
 
-// ODrive.Controller.ControlMode
-enum ControlMode {
-    CONTROL_MODE_VOLTAGE_CONTROL             = 0,
-    CONTROL_MODE_TORQUE_CONTROL              = 1,
-    CONTROL_MODE_VELOCITY_CONTROL            = 2,
-    CONTROL_MODE_POSITION_CONTROL            = 3,
-};
-
-// ODrive.Controller.InputMode
-enum InputMode {
-    INPUT_MODE_INACTIVE                      = 0,
-    INPUT_MODE_PASSTHROUGH                   = 1,
-    INPUT_MODE_VEL_RAMP                      = 2,
-    INPUT_MODE_POS_FILTER                    = 3,
-    INPUT_MODE_MIX_CHANNELS                  = 4,
-    INPUT_MODE_TRAP_TRAJ                     = 5,
-    INPUT_MODE_TORQUE_RAMP                   = 6,
-    INPUT_MODE_MIRROR                        = 7,
-    INPUT_MODE_TUNING                        = 8,
-};
+#define ODRIVE_VELOCITY_LIMIT (15.0)
+#define ODRIVE_CURRENT_LIMIT (70.0)
