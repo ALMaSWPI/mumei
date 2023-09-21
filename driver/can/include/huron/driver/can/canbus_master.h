c5341c974318e9c33261e9ac0d037d4ad7ea51ce
Duc Doan <dtbpkmte@gmail.com>
2023-09-20 21:43:57 -0400

    Fix

--- driver/can/include/huron/driver/can/canbus_master.h
+++ driver/can/include/huron/driver/can/canbus_master.h
@@ -3,12 +3,12 @@
 #include "canbus.h"
 
 class CanBusMasterBase : public CanBusBase {
-  public:
-    /**
-      * @brief Receives a CAN message
-      * 
-      * @returns: true on success or false otherwise (e.g. if the send queue is
-      * full).
-      */
-    virtual bool receive_message(const can_Message_t& message) = 0;
+ public:
+   /**
+     * @brief Receives a CAN message
+     * 
+     * @returns: true on success or false otherwise (e.g. if the send queue is
+     * full).
+     */
+   virtual bool receive_message(const can_Message_t& message) = 0;
 };
