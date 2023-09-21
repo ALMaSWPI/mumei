1d72f02424652857f2ba22a58a8966ed6d2be467
Duc Doan <dtbpkmte@gmail.com>
2023-09-21 15:26:43 -0400

    Add class MovingGroupComponent

---
+++ system/control_interfaces/include/huron/control_interfaces/moving_group_component.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <vector>
+#include "generic_component.h"
+
+namespace huron {
+
+class MovingGroupComponent: public GenericComponent{
+    virtual bool Move(std::vector<float> vals) = 0;
+    
+    virtual bool Stop() = 0;
+};
+
+}
