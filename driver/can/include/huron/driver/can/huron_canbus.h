fd8defefa2f2d9a8a7af60a29b70f025bca7163e
Duc Doan <dtbpkmte@gmail.com>
2023-09-20 21:53:38 -0400

    Fix

--- driver/can/include/huron/driver/can/huron_canbus.h
+++ driver/can/include/huron/driver/can/huron_canbus.h
@@ -17,38 +17,38 @@ enum {
 };
 
 class HURONCanBus : public CanBusBase {
-  public:
-    static constexpr sockcanpp::milliseconds kRecvTimeout{1000};
-    // struct Config_t {
-    // 	uint32_t baud_rate = CAN_BAUD_250K;
-    // 	Protocol protocol = PROTOCOL_SIMPLE;
-    //
-    // 	HURONCanBus* parent = nullptr; // set in apply_config()
-    // 	void set_baud_rate(uint32_t value) { parent->set_baud_rate(value); }
-    // };
+ public:
+  static constexpr sockcanpp::milliseconds kRecvTimeout{1000};
+  // struct Config_t {
+  // 	uint32_t baud_rate = CAN_BAUD_250K;
+  // 	Protocol protocol = PROTOCOL_SIMPLE;
+  //
+  // 	HURONCanBus* parent = nullptr; // set in apply_config()
+  // 	void set_baud_rate(uint32_t value) { parent->set_baud_rate(value); }
+  // };
 
-    HURONCanBus(std::string can_if, uint32_t axis_id)
-      : can_if_(can_if), axis_id_(axis_id), recv_timeout_(kRecvTimeout) {}
+  HURONCanBus(std::string can_if, uint32_t axis_id)
+    : can_if_(can_if), axis_id_(axis_id), recv_timeout_(kRecvTimeout) {}
 
-    std::string can_if_;
-    uint32_t axis_id_;
-    sockcanpp::milliseconds recv_timeout_;
-    sockcanpp::CanDriver can_driver_{can_if_, CAN_RAW};
+  std::string can_if_;
+  uint32_t axis_id_;
+  sockcanpp::milliseconds recv_timeout_;
+  sockcanpp::CanDriver can_driver_{can_if_, CAN_RAW};
 
-    static const uint8_t kCanFifoNone = 0xff;
+  static const uint8_t kCanFifoNone = 0xff;
 
-    struct ODriveCanSubscription : CanSubscription {
-      uint8_t fifo = kCanFifoNone;
-      on_can_message_cb_t callback;
-      void* ctx;
-    };
+  struct ODriveCanSubscription : CanSubscription {
+    uint8_t fifo = kCanFifoNone;
+    on_can_message_cb_t callback;
+    void* ctx;
+  };
 
-    bool send_message(const can_Message_t& message) final;
-    bool recv_message(can_Message_t& message,
-		      uint32_t timeout = UINT32_MAX) final;
-    bool subscribe(const MsgIdFilterSpecs& filter,
-		   on_can_message_cb_t callback, void* ctx,
-		   CanSubscription** handle) final;
-    bool unsubscribe(CanSubscription* handle) final;
+  bool send_message(const can_Message_t& message) final;
+  bool recv_message(can_Message_t& message,
+		    uint32_t timeout = UINT32_MAX) final;
+  bool subscribe(const MsgIdFilterSpecs& filter,
+		 on_can_message_cb_t callback, void* ctx,
+		 CanSubscription** handle) final;
+  bool unsubscribe(CanSubscription* handle) final;
 };
 
