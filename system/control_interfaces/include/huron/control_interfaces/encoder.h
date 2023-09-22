9e921fa8be727c5dbba9e1dc4f52b45a4e4a4fce
Duc Doan <dtbpkmte@gmail.com>
2023-09-21 21:20:10 -0400

    Add documentation for Encoder

--- system/control_interfaces/include/huron/control_interfaces/encoder.h
+++ system/control_interfaces/include/huron/control_interfaces/encoder.h
@@ -5,6 +5,11 @@
 
 namespace huron {
 
+/**
+ * Abstract class for using an encoder.
+ *
+ * 
+ */
 class Encoder : public GenericComponent {
 private:
     float count_;
@@ -17,33 +22,64 @@ public:
     Encoder& operator=(const Encoder&) = delete;
     ~Encoder() = default;
 
-    void  Reset() {
+    /**
+     * Resets the encoder count.
+     */
+    void Reset() {
         count_ = 0.0;
         prev_count_ = 0.0;
     }
 
+    /**
+     * Gets the current encoder count.
+     */
     virtual float GetCount() = 0;
 
+    /**
+     * Gets the previous encoder count.
+     */
     float GetPrevCount() {
         return prev_count_;
     }
 
+    /**
+     * Gets the counts per revolution (CPR).
+     */
     float GetCPR() {
         return cpr_;
     }
 
+    /**
+     * Gets the current angle in radians.
+     */
     float GetAngleRadian() {
         return count_ / cpr_ * 2.0 * M_PI;
     }
 
+    /**
+     * Gets the current angle in degrees.
+     */
     float GetAngleDegree() {
       return count_ / cpr_ * 360.0;
     }
 
+    /**
+     * Gets the current velocity in counts.
+     */
     virtual float GetVelocity() = 0;
 
+    /**
+     * Gets the current velocity in radians.
+     */
     float GetVelocityRadian() {
         return GetVelocity() / cpr_ * 2 * M_PI;
+    }
+
+    /**
+     * Gets the current velocity in degrees.
+     */
+    float GetVelocityDegree() {
+        return GetVelocity() / cpr_ * 360.0;
     }
 };
 
