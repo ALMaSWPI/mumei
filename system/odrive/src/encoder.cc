bf867412d5a71281bcf08550cbbbcdfc51c3ce83
Duc Doan <dtbpkmte@gmail.com>
2023-09-22 15:52:01 -0400

    Fixed and added odrive encoder test

--- system/odrive/src/encoder.cc
+++ system/odrive/src/encoder.cc
@@ -7,6 +7,23 @@ Encoder::Encoder(float cpr,
                  std::shared_ptr<HuronODriveCAN> odrive)
   : huron::Encoder(cpr), odrive_(std::move(odrive)) {}
 
+void Encoder::Configure() {
+
+}
+
+void Encoder::Initialize() {
+
+}
+
+void Encoder::SetUp() {
+
+}
+
+void Encoder::Terminate() {
+
+}
+
+
 float Encoder::GetCount() {
   prev_count_ = count_;
   odrive_->GetEncoderEstimates(count_, velocity_);
