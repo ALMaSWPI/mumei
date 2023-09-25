b5ff2e006086a8743c6aeabc1c0be618967019b0
Duc Doan <dtbpkmte@gmail.com>
2023-09-25 14:03:18 -0400

    Test RTR bit

--- driver/can/src/huron_canbus.cc
+++ driver/can/src/huron_canbus.cc
@@ -7,6 +7,10 @@
 bool HURONCanBus::send_message(const can_Message_t &tx_msg) {
   struct can_frame raw_frame;
   raw_frame.can_id = tx_msg.id;
+  // Handle RTR bit
+  if (tx_msg.rtr) {
+    raw_frame.can_id |= CAN_RTR_FLAG;
+  }
   raw_frame.can_dlc = tx_msg.len;
   std::memcpy(raw_frame.data, tx_msg.buf, tx_msg.len);
   sockcanpp::CanMessage msg_to_send(raw_frame);
