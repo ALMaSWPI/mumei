e8b846bfac74176e770ae6e57c7ccbd693f2018b
NhiNguyenCMT8 <nguyenngocyennhihcmvn@gmail.com>
2023-09-26 18:08:41 -0400

    formatting fix

--- system/control_interfaces/include/huron/control_interfaces/encoder.h
+++ system/control_interfaces/include/huron/control_interfaces/encoder.h
@@ -1,23 +1,23 @@
 #pragma once
 #include "generic_component.h"
 
-namespace huron{
+namespace huron {
 /**
  * Abstract class for encoder
  * A generic encoder has count and velocity.
  *
  * @ingroup control_interface
  */
- class Encoder : public GenericComponent {
-  public:
-   Encoder() = default;
-   Encoder(const Encoder&) = delete;
-   Encoder& operator=(const Encoder&) = delete;
-   virtual ~Encoder() = default;
+  class Encoder : public GenericComponent {
+   public:
+    Encoder() = default;
+    Encoder(const Encoder&) = delete;
+    Encoder& operator=(const Encoder&) = delete;
+    virtual ~Encoder() = default;
 
-   virtual float GetPosition() = 0;
-   virtual float GetVelocity() = 0;
-   virtual void Reset() = 0;
- };
+    virtual float GetPosition() = 0;
+    virtual float GetVelocity() = 0;
+    virtual void Reset() = 0;
+  };
 
-}
+}  // namespace huron
