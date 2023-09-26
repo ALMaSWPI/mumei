8149980fd7f9068c6026507431332653b01d387f
NhiNguyenCMT8 <nguyenngocyennhihcmvn@gmail.com>
2023-09-26 18:18:16 -0400

    formatting fix

--- system/control_interfaces/include/huron/control_interfaces/encoder.h
+++ system/control_interfaces/include/huron/control_interfaces/encoder.h
@@ -9,16 +9,16 @@ namespace huron {
  *
  * @ingroup control_interface
  */
-  class Encoder : public GenericComponent {
-   public:
-    Encoder() = default;
-    Encoder(const Encoder&) = delete;
-    Encoder& operator=(const Encoder&) = delete;
-    virtual ~Encoder() = default;
+class Encoder : public GenericComponent {
+ public:
+  Encoder() = default;
+  Encoder(const Encoder&) = delete;
+  Encoder& operator=(const Encoder&) = delete;
+  virtual ~Encoder() = default;
 
-    virtual float GetPosition() = 0;
-    virtual float GetVelocity() = 0;
-    virtual void Reset() = 0;
-  };
+  virtual float GetPosition() = 0;
+  virtual float GetVelocity() = 0;
+  virtual void Reset() = 0;
+};
 
 }  // namespace huron
