2c82e51a57be3271521856b9cdd7cc584effeb31
Duc Doan <dtbpkmte@gmail.com>
2023-10-05 18:16:50 -0400

    Exception headers were swapped

--- system/exceptions/include/huron/exceptions/not_implemented_exception.h
+++ system/exceptions/include/huron/exceptions/not_implemented_exception.h
@@ -8,24 +8,23 @@
 
 namespace huron {
 
-class InvalidConfigurationException : public std::logic_error {
+class NotImplementedException : public std::logic_error {
  private:
   std::string _text;
 
-  InvalidConfigurationException(const char* message, const char* function)
-      : std::logic_error("Invalid Configuration provided.") {
+  NotImplementedException(const char* message, const char* function)
+      : std::logic_error("Not Implemented") {
     _text = message;
     _text += " : ";
     _text += function;
   }
 
  public:
-  InvalidConfigurationException()
-      : InvalidConfigurationException(
-            "Invalid Configuration provided.", __FUNCTION__) {}
+  NotImplementedException()
+      : NotImplementedException("Not Implememented", __FUNCTION__) {}
 
-  explicit InvalidConfigurationException(const char* message)
-      : InvalidConfigurationException(message, __FUNCTION__) {}
+  explicit NotImplementedException(const char* message)
+      : NotImplementedException(message, __FUNCTION__) {}
 
   virtual const char *what() const throw() {
       return _text.c_str();
