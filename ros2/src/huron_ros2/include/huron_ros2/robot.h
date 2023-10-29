dcf10c5dd2bf0982f104537ba2e7ac155bb529f4
Duc Doan <dtbpkmte@gmail.com>
2023-10-29 12:32:15 -0400

    Fix formatting

--- ros2/src/huron_ros2/include/huron_ros2/robot.h
+++ ros2/src/huron_ros2/include/huron_ros2/robot.h
@@ -9,17 +9,21 @@
 #include "huron/control_interfaces/generic_component.h"
 #include "huron/control_interfaces/moving_group_component.h"
 
-
 namespace huron {
 namespace ros2 {
+
 class HuronNode : public rclcpp::Node {
- public:// init
+ public:
   HuronNode() : Node("node_handler") {
-    //TODO: Correct the topics and their msg type here
-    //All the subscriber(s)
-    subscription_joints_ = this->create_subscription<sensor_msgs::msg::JointState>(
-      "joint_states", 10, std::bind(&HuronNode::topic_callback_subscription_joints, this, std::placeholders::_1));
-  };
+    subscription_joints_ =
+      this->create_subscription<sensor_msgs::msg::JointState>(
+        "joint_states",
+        10,
+        std::bind(
+          &HuronNode::topic_callback_subscription_joints,
+          this,
+          std::placeholders::_1));
+  }
 
   void topic_callback_subscription_joints(
     std::shared_ptr<const sensor_msgs::msg::JointState> msg) const {
@@ -29,23 +33,23 @@ class HuronNode : public rclcpp::Node {
     }
     RCLCPP_INFO(this->get_logger(), "\n");
   }
-  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr subscription_joints_;
+
+ private:
+  rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr
+    subscription_joints_;
 };
 
 class Robot : public huron::MovingGroupComponent {
  public:
-  Robot(){};
-  HuronNode node;
-  void Configure();
-  void Terminate();
-  void SetUp(){};
-  bool Move(std::vector<float> values);
-  bool MoveVelocity(std::vector<float> values);
-  bool MoveTorque(std::vector<float> values);
+  void Configure() override;
+  void Terminate() override;
+  void SetUp() override;
 
-  bool Stop();
+  bool Move(std::vector<float> values) override;
+  bool Stop() override;
 
  private:
+  HuronNode node;
 };
 
 
