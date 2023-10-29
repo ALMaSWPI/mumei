b9b193aec1ecd7382c4222980aa1f1def217aba4
Duc Doan <dtbpkmte@gmail.com>
2023-10-29 12:32:15 -0400

    Fix formatting

--- ros2/src/huron_ros2/src/robot.cc
+++ ros2/src/huron_ros2/src/robot.cc
@@ -25,30 +25,6 @@ bool Robot::Move(std::vector<float> values){
   return true;
 }
 
-bool Robot::MoveVelocity(std::vector<float> values){
-  // Publishing to the topic
-  auto message = sensor_msgs::msg::JointState();
-  for (int i = 0; i < 3; i++){
-    message.velocity[i] = values[i];
-  }
-  // RCLCPP_INFO(this->node.get_logger(), "Publishing Velocity:");
-//  RCLCPP_INFO(message.velocity);
-  // this->node.publisher_joints_->publish(message);
-  return true;
-}
-
-bool Robot::MoveTorque(std::vector<float> values){
-  // Publishing to the topic
-  auto message = sensor_msgs::msg::JointState();
-  for (int i = 0; i < 3; i++){
-    message.effort[i] = values[i];
-  }
-  RCLCPP_INFO(this->node.get_logger(), "Publishing Torque");
-//  RCLCPP_INFO(message.effort);
-  // this->node.publisher_joints_->publish(message);
-  return true;
-}
-
 bool Robot::Stop(){
   // Publishing to the topic
   auto message = sensor_msgs::msg::JointState();
