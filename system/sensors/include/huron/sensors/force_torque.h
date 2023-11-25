a1d6c4e7194370195efed2434c127e9748677ae1
Duc Doan <dtbpkmte@gmail.com>
2023-11-25 01:47:06 -0500

    Add ZMP FT sensor test, ZMPFTSensor calculation correct

--- system/sensors/include/huron/sensors/force_torque.h
+++ system/sensors/include/huron/sensors/force_torque.h
@@ -2,18 +2,66 @@
 
 #include <eigen3/Eigen/Core>
 
+#include "huron/control_interfaces/generic_component.h"
 #include "huron/types.h"
 
 namespace huron {
 
-class ForceTorqueSensor {
+class ForceTorqueSensorConfiguration : public Configuration {
  public:
+  ForceTorqueSensorConfiguration(
+    ConfigMap config_map,
+    std::set<std::string> valid_keys)
+      : Configuration(config_map, [&valid_keys]() {
+                        std::set<std::string> tmp(kFtSensorValidKeys);
+                        tmp.merge(valid_keys);
+                        return tmp;
+                      }()) {}
+
+  explicit ForceTorqueSensorConfiguration(ConfigMap config_map)
+      : ForceTorqueSensorConfiguration(config_map, {}) {}
+
+  ForceTorqueSensorConfiguration()
+      : ForceTorqueSensorConfiguration({}, {}) {}
+
+ private:
+  static const inline std::set<std::string> kFtSensorValidKeys{};
+
+};
+
+class ForceTorqueSensor : public GenericComponent {
+ public:
+  ForceTorqueSensor(
+    std::string name,
+    bool reverse_wrench_direction,
+    std::unique_ptr<ForceTorqueSensorConfiguration> config)
+    : GenericComponent(std::move(config)),
+      name_(name),
+      reverse_wrench_direction_(reverse_wrench_direction) {}
+
+  ForceTorqueSensor(std::string name, bool reverse_wrench_direction = false)
+    : ForceTorqueSensor(name,
+                        reverse_wrench_direction,
+                        std::make_unique<ForceTorqueSensorConfiguration>()) {}
   /**
    * Measures the external forces and moments.
    *
    * @return Wrench 6x1 vector \f$ [Fx, Fy, Fz, Tx, Ty, Tz]^T \f$.
    */
-  virtual Vector6d GetWrench() = 0;
+  Vector6d GetWrench() {
+    if (reverse_wrench_direction_)
+      return -GetWrenchRaw();
+    return GetWrenchRaw();
+  }
+  
+ protected:
+  /**
+   * To be overriden.
+   */
+  virtual Vector6d GetWrenchRaw() = 0;
+
+  std::string name_;
+  bool reverse_wrench_direction_;
 };
 
 }  // namespace huron
