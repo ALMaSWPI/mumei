9f7e330bc8f872c93f9c0706e390e8498d83e731
Duc Doan <dtbpkmte@gmail.com>
2023-12-31 00:24:15 -0500

    Add convenient types

--- common/include/huron/types.h
+++ common/include/huron/types.h
@@ -5,5 +5,7 @@
 namespace huron {
 
 typedef Eigen::Matrix< double, 6, 1 > Vector6d;
+typedef Eigen::Matrix< double, 6, 6 > Matrix6d;
+typedef Eigen::Matrix< double, 6, Eigen::Dynamic > Matrix6Xd;
 
 }  // namespace huron
