b1f57e1f34af86982d6e99e0835e4709ce9e814a
Duc Doan <dtbpkmte@gmail.com>
2024-01-05 23:33:17 -0500

    Add copy, assign, destructors to ZMP

--- system/locomotion/include/huron/locomotion/zero_moment_point_fsr_array.h
+++ system/locomotion/include/huron/locomotion/zero_moment_point_fsr_array.h
@@ -10,6 +10,9 @@ class ZeroMomentPointFSRArray : public ZeroMomentPoint {
     std::weak_ptr<const multibody::Frame> zmp_frame,
     double normal_force_threshold,
     std::shared_ptr<ForceSensingResistorArray> fsr_array);
+  ZeroMomentPointFSRArray(const ZeroMomentPointFSRArray&) = delete;
+  ZeroMomentPointFSRArray& operator=(const ZeroMomentPointFSRArray&) = delete;
+  ~ZeroMomentPointFSRArray() override = default;
 
   Eigen::Vector2d Eval(double& fz) override;
 
--- system/locomotion/include/huron/locomotion/zero_moment_point_ft_sensor.h
+++ system/locomotion/include/huron/locomotion/zero_moment_point_ft_sensor.h
@@ -10,6 +10,9 @@ class ZeroMomentPointFTSensor : public ZeroMomentPoint {
     std::weak_ptr<const multibody::Frame> zmp_frame,
     double normal_force_threshold,
     const std::vector<std::shared_ptr<ForceTorqueSensor>>& ft_sensors);
+  ZeroMomentPointFTSensor(const ZeroMomentPointFTSensor&) = delete;
+  ZeroMomentPointFTSensor& operator=(const ZeroMomentPointFTSensor&) = delete;
+  ~ZeroMomentPointFTSensor() override = default;
 
   Eigen::Vector2d Eval(double& fz) override;
 
