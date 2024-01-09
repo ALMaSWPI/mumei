6325e2e543ebca9583a27c8d6d2cf87bcd736e41
Duc Doan <dtbpkmte@gmail.com>
2024-01-08 21:51:13 -0500

    Port huron_ros2 to new API

---
+++ ros2/src/huron_ros2/include/huron_ros2/joint_group_controller.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "huron/control_interfaces/moving_interface.h"
+
+namespace huron {
+namespace ros2 {
+
+class HuronNode;
+
+class JointGroupController : public huron::MovingInterface {
+ public:
+  static constexpr size_t kNumActuators = 12;
+
+  explicit JointGroupController(std::shared_ptr<HuronNode> node);
+  JointGroupController(const JointGroupController&) = delete;
+  JointGroupController& operator=(const JointGroupController&) = delete;
+  ~JointGroupController() override = default;
+
+  bool Move(const std::vector<double>& values) override;
+  bool Move(const Eigen::VectorXd& values) override;
+  bool Stop() override;
+
+ private:
+  std::shared_ptr<HuronNode> node_;
+};
+
+}  // namespace ros2
+}  // namespace huron
