08d1caa7fbde481392160d4523da4757beb8109b
Duc Doan <dtbpkmte@gmail.com>
2024-01-23 15:22:21 -0500

    Fix bug on Docker image

--- tools/build_huron.sh
+++ tools/build_huron.sh
@@ -6,8 +6,8 @@ echo "Target platform: $1"
 
 #Build huron
 cd /huron
-export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/opt/ros/humble/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+echo $LD_LIBRARY_PATH
 cmake -DBUILD_TYPE=$1 -DUSE_PINOCCHIO=1 -DCMAKE_PREFIX_PATH=/opt/ros/humble -Bbuild
 cmake --build build --parallel --target install
-cd build && ctest
-ctest --rerun-failed --output-on-failure
+cd build && ctest --output-on-failure
