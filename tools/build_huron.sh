48dc4f637a2e53f078318817f939b2cba7344fd4
Duc Doan <dtbpkmte@gmail.com>
2024-01-23 15:38:11 -0500

    Add aarch64 to ld path

--- tools/build_huron.sh
+++ tools/build_huron.sh
@@ -6,7 +6,7 @@ echo "Target platform: $1"
 
 #Build huron
 cd /huron
-export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/opt/ros/humble/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib:/opt/ros/humble/lib/x86_64-linux-gnu:/opt/ros/humble/lib/aarch64-linux-gnu:$LD_LIBRARY_PATH
 echo $LD_LIBRARY_PATH
 cmake -DBUILD_TYPE=$1 -DUSE_PINOCCHIO=1 -DCMAKE_PREFIX_PATH=/opt/ros/humble -Bbuild
 cmake --build build --parallel --target install
